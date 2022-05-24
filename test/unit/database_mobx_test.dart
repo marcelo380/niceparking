43ef9a4f59610042b9f83f2bd3ca811802b05632
Marcelo Roberto <marcelo@marcelo.com>
2022-05-24 17:03:37 -0300

    test: implementa testes do mobx e database
    Testa criação da tabela, listagem mobx, inserção mobx

---
+++ test/unit/database_mobx_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nice_parking/controllers/parking_mobx_ctrl/parking_mobx_ctrl.dart';
+import 'package:nice_parking/data/database_helper.dart';
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  Database? db;
+  ParkingMobxCTRL? parkingMobxCTRL = ParkingMobxCTRL();
+  sqfliteFfiInit();
+  setUpAll(() async {
+    sqfliteFfiInit();
+    db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
+    //criar tabela base, isso vai garantir que o sql seja o mesmo
+    await db!.execute(queryDBCreate);
+
+    //mocka a db
+    parkingMobxCTRL.mockDatabase = db;
+
+    //inicia
+    await parkingMobxCTRL.init();
+  });
+
+  test('Testa database do sqlite', () async {
+    expect(await db!.getVersion(), 0);
+  });
+
+  test('Teste inicialização das vagas ', () async {
+    expect(parkingMobxCTRL.parkingSlotsList.length, 10);
+  });
+
+  test('Teste inserção de veiculo na vaga', () async {
+    await parkingMobxCTRL.insertVehicleParkingSlot(
+        numVaga: 1, responsavel: 'marcelo');
+    await parkingMobxCTRL.insertVehicleParkingSlot(
+        numVaga: 2, responsavel: 'roberto');
+
+    List _testInsert = parkingMobxCTRL.parkingSlotsList
+        .where((e) => e.empty == false)
+        .toList();
+
+    expect(_testInsert.length, 2);
+  });
+}
