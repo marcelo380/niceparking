21fbc546da8f52e48781172c70cae6f73e6eab03
Marcelo Roberto <marcelo@marcelo.com>
2022-05-25 16:07:14 -0300

    chore: implementa constantes

---
+++ lib/utils/consts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color greenPrimary1 = Color(0xFF03DABB);
+const Color greenPrimary2 = Color(0xFF03DA9A);
+const Color greenSecondary = Color(0xFF006657);
+const Color customBlack = Color(0xFF0C1615);
+const Color black2 = Color(0xFF162826);
