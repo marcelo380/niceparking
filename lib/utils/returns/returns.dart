9b75da576265ac096caa7f627decd69a27c31364
Marcelo Roberto <marcelo@marcelo.com>
2022-05-25 10:46:41 -0300

    refactor: ajusta esquema para retornos padrão do db

---
+++ lib/utils/returns/returns.dart
@@ -0,0 +1,17 @@
+import 'package:sqflite/sqlite_api.dart';
+
+ReturnMessage returnError(String message, {DatabaseException? error}) {
+  print("exception => $error");
+  var result = ReturnMessage(false, message, null);
+  return result;
+}
+
+ReturnMessage returnSucess(String message, {dynamic data}) =>
+    ReturnMessage(true, message, data);
+
+class ReturnMessage {
+  bool sucess;
+  String message;
+  dynamic data;
+  ReturnMessage(this.sucess, this.message, this.data);
+}
