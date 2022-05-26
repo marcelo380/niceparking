8dfc73a8bde29a4685bf44d63b032a97f8c5b388
Marcelo Roberto <marcelo@marcelo.com>
2022-05-26 17:08:17 -0300

    feat: implementa componente de alerta

---
+++ lib/components/confirm_dialog/alert_dialog.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+Future<bool> showAlertDialog({
+  required BuildContext context,
+  required String title,
+  required String content,
+  String? cancelActionText,
+  required String defaultActionText,
+}) async {
+  if (Platform.isAndroid) {
+    return await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          if (cancelActionText != null)
+            TextButton(
+              child: Text(cancelActionText),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+          TextButton(
+            child: Text(defaultActionText),
+            onPressed: () => Navigator.of(context).pop(true),
+          ),
+        ],
+      ),
+    );
+  } else if (Platform.isIOS) {
+    return await showCupertinoDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(content),
+              actions: <Widget>[
+                if (cancelActionText != null)
+                  CupertinoDialogAction(
+                    child: Text(cancelActionText),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                CupertinoDialogAction(
+                  child: Text(defaultActionText),
+                  onPressed: () => Navigator.of(context).pop(true),
+                ),
+              ],
+            ));
+  } else {
+    return false;
+  }
+}
--- lib/components/contained_button/contained_button.dart
+++ lib/components/contained_button/contained_button.dart
@@ -5,15 +5,19 @@ import 'package:nice_parking/utils/consts.dart';
 class ContainedButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
+  final Color overideColor;
   const ContainedButton(
-      {required this.label, required this.onPressed, Key? key})
+      {required this.label,
+      required this.onPressed,
+      this.overideColor = greenPrimary1,
+      Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(greenPrimary1),
+        backgroundColor: MaterialStateProperty.all<Color>(overideColor),
       ),
       onPressed: onPressed,
       child: CustomTypography.title16(label, color: blackText),
